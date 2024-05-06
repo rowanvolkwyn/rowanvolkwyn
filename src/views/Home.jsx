de71b7b02ff19077fc7a0a22bfb80816c87fb244
Rowan <volkwyn.rowan@gmail.com>
2024-05-06 12:56:15 +1000

    first commit

---
+++ src/views/Home.jsx
@@ -0,0 +1,14 @@
+function Home() {
+    return (
+        <>
+            <p><strong>Hi, my name is</strong></p>
+            <h1>Rowan Volkwyn</h1>
+            <h2>and I'm a Software Engineer</h2>
+            <p>I love building <strong>fullstack apps</strong>that <strong>delight the user</strong>
+             and <strong>provide tremendous value.</strong> I'm currently looking to join a team of likeminded people 
+             to help solve issues that I'm passionate about.</p>
+        </>
+    )
+}
+
+export default Home;
