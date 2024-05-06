9f846c980f805cbb082162b9b0d325142ae74a58
Rowan <volkwyn.rowan@gmail.com>
2024-05-06 13:24:35 +1000

    created home page

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,13 +1,16 @@
+import './Home.css';
+
 function Home() {
     return (
-        <>
+        <div id='container'>
             <p><strong>Hi, my name is</strong></p>
             <h1>Rowan Volkwyn</h1>
-            <h2>and I'm a Software Engineer</h2>
-            <p>I love building <strong>fullstack apps</strong>that <strong>delight the user</strong>
-             and <strong>provide tremendous value.</strong> I'm currently looking to join a team of likeminded people 
+            <h2>and I'm a Software Engineer.</h2>
+            <p>I love building <strong>fullstack apps</strong> that<strong> delight the user </strong>
+              and <strong>provide tremendous value.</strong> <br></br>I'm currently looking to join a team of like-minded people 
              to help solve issues that I'm passionate about.</p>
-        </>
+             <button>Check out my Resume!</button>
+        </div>
     )
 }
 
