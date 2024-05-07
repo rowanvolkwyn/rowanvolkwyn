fbed08b97455cba84db473d348a5070812824e7a
Rowan <volkwyn.rowan@gmail.com>
2024-05-07 22:46:25 +1000

    added styling for Projects view

--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -1,14 +1,21 @@
+import './Projects.css';
+
 function Projects() {
     return (
         <div id="project-content">
-            <p><strong>Featured Project</strong></p>
-            <h3>Peak Fitness App</h3>
-            <div id='synopsis-container'>
-                <p>
-                    A fitness app used to track personal bests and help set achievable goals for each workout.
-                    This was created using <strong>React, Node.js, Express.js</strong> and <strong>MongoDB.</strong> I also
-                    created and published an <strong>iOS App</strong> using <strong>Swift.</strong> 
-                </p>
+            <div id='left'>
+
+            </div>
+            <div id="right">
+                <p><strong>Featured Project</strong></p>
+                <h3>Peak Fitness App</h3>
+                <div id='synopsis-container'>
+                    <p id="synopsis">
+                        A fitness app used to track personal bests and help set achievable goals for each workout.
+                        This was created using <strong>React, Node.js, Express.js</strong> and <strong>MongoDB.</strong> I also
+                        created and published an <strong>iOS App</strong> using <strong>Swift.</strong> 
+                    </p>
+                </div>
             </div>
         </div>
     )
