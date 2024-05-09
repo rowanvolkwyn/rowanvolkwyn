3132a16435f2ae780456c53cfdccdb9f69d4aaea
Rowan <volkwyn.rowan@gmail.com>
2024-05-09 22:07:58 +1000

    implemented routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import './App.css';
 import Home from './views/Home';
 import Experience from './views/Experience';
 import Projects from './views/Projects';
+import { BrowserRouter, Routes, Route, NavLink } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-        <Home />
-        <Experience />
-        <Projects />
-    </div>
-
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='/experience' element={<Experience />} />
+        <Route path='projects' element={<Projects />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
